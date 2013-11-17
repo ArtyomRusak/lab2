6beaf3930962d06ada0f41364652c140656c9d10
ArtyomRusak <bembi1204@gmail.com>
2013-11-18 00:24:44 +0300

    [Added]: Starting implement lab2. [ArtyomRusak]

---
+++ SongExercise/SongExercise/Playlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongExercise
+{
+    public class Playlist
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan FullDuration
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Playlist()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
